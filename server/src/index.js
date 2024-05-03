90f9c7efe9a16221e754b8973c0cf50416278a4a
Mehmood <mhmd@Hmz-MBP.local>
2024-05-03 15:25:08 +0500

    Server and client folders added

---
+++ server/src/index.js
@@ -0,0 +1,40 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { v4 as uuidv4 } from "uuid";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("Authentication error"));
+  }
+  socket.username = username;
+  socket.userId = uuidv4();
+  next();
+});
+io.on("connection", async (socket) => {
+  console.log("a user connected");
+  // all connected users
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({ userId: id, username: socket.username });
+  }
+
+  // all users event
+  socket.emit("users", users);
+  // connected user details
+  socket.emit("session", { userId: socket.userId, username: socket.username });
+  // socket.on('disconnect', () => {
+  //     console.log('user disconnected')
+  // })
+});
+
+console.log("Listening on port 4000");
+httpServer.listen(process.env.PORT || 4000);
